cd3b2d217f61feb07d90cdb1fc4b7e02ff25f683
Fabricio santuchi <131098765+Fabricio-santuchi@users.noreply.github.com>
2024-03-03 23:36:19 -0300

    sv

--- app/validacao.js
+++ app/validacao.js
@@ -11,6 +11,18 @@ function verificaSeOChutePossuiUmVaLorValido(chute){
         return
     }
 
+    if(chute.toUpperCase() === 'GAME OVER'){
+        document.body.innerHTML =`
+            <h2>Game Over!!!</h2>
+            <h3>Pressione o botão para jogar novamente</h3>
+            <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+        `
+            document.body.style.backgroundColor = "black";
+        } else {
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
+    }
+
     if(numero < numeroSecreto){
         elementoChute.innerHTML += `
             <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div> `;
