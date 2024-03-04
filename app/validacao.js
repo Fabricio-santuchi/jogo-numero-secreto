8741e05e6f712888deb9869c52313bc57b8283a6
Fabricio santuchi <131098765+Fabricio-santuchi@users.noreply.github.com>
2024-03-03 23:40:05 -0300

    sv

--- app/validacao.js
+++ app/validacao.js
@@ -2,25 +2,22 @@ function verificaSeOChutePossuiUmVaLorValido(chute){
     const numero = +chute
 
     if(chuteForInvalido(numero)){
-        elementoChute.innerHTML += '<div>valor Inválido!</div>';
+        if(chute.toUpperCase() === 'GAME OVER'){
+            document.body.innerHTML =`
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+            `
+            document.body.style.backgroundColor = "black";
+        }else{
+            elementoChute.innerHTML += '<div>valor Inválido!</div>';
+        }
         return
     }
 
     if(numeroForMaiorOuMenorQueOValorPermitido(numero)){
         elementoChute.innerHTML += `<div>valor Inválido: Fale um valor entre ${menorValor} e ${maiorValor}</div>.`;
         return
-    }
-
-    if(chute.toUpperCase() === 'GAME OVER'){
-        document.body.innerHTML =`
-            <h2>Game Over!!!</h2>
-            <h3>Pressione o botão para jogar novamente</h3>
-            <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
-        `
-            document.body.style.backgroundColor = "black";
-        } else {
-            elementoChute.innerHTML += '<div>Valor Inválido</div>';
-        }
     }
 
     if(numero < numeroSecreto){
